63a8a01a0c77018d2e11098b9efacdcd40c02908
Sejin Kim <sejin.kim1904@gmail.com>
2020-05-26 14:12:23 -0600

    Create card validation runner

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,37 +1,11 @@
-card_number = "5541808923795240"
+require './lib/credit_card'
 
-def luhn(card)
-  if sum_digits(card).reduce(:+) % 10 == 0
-    print "The number #{card} is valid!"
-  else
-    print "The number #{card} is invalid!"
-  end
+puts "Enter a credit card number you wish to validate:"
+
+card = CreditCard.new(gets.chomp)
+
+if card.is_valid?
+  puts "The number #{card.card_number} is valid!"
+else
+  puts "The number #{card.card_number} is invalid!"
 end
-
-def split_to_integer(card)
-  card.chars.map do |digit|
-    digit.to_i
-  end
-end
-
-def multiply_digits(card)
-  split_to_integer(card).map.with_index do |digit, index|
-    if index == 0 || index % 2 == 0
-      digit * 2
-    else
-      digit
-    end
-  end
-end
-
-def sum_digits(card)
-  multiply_digits(card).map do |digit|
-    if digit.to_s.length == 2
-      digit.to_s.chars.map { |num| num.to_i }.reduce(:+)
-    else
-      digit
-    end
-  end
-end
-
-luhn(card_number)
