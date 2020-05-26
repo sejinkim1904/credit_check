05c06e816fddc41bc9d4130e56269e557cf094af
Sejin Kim <sejin.kim1904@gmail.com>
2020-05-26 12:31:19 -0600

    Refactor methods to chain eachother

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,14 +1,7 @@
-require 'pry'
-
 card_number = "5541808923795240"
 
-# Your Luhn Algorithm Here
 def luhn(card)
-  card_split = split_to_integer(card)
-  multiplied_digits = multiply_digits(card_split)
-  summed_digits = sum_digits(multiplied_digits)
-
-  if summed_digits.reduce(:+) % 10 == 0
+  if sum_digits(card).reduce(:+) % 10 == 0
     print "The number #{card} is valid!"
   else
     print "The number #{card} is invalid!"
@@ -21,8 +14,8 @@ def split_to_integer(card)
   end
 end
 
-def multiply_digits(digits)
-  digits.map.with_index do |digit, index|
+def multiply_digits(card)
+  split_to_integer(card).map.with_index do |digit, index|
     if index == 0 || index % 2 == 0
       digit * 2
     else
@@ -31,8 +24,8 @@ def multiply_digits(digits)
   end
 end
 
-def sum_digits(digits)
-  digits.map do |digit|
+def sum_digits(card)
+  multiply_digits(card).map do |digit|
     if digit.to_s.length == 2
       digit.to_s.chars.map { |num| num.to_i }.reduce(:+)
     else
@@ -42,6 +35,3 @@ def sum_digits(digits)
 end
 
 luhn(card_number)
-# Output
-## If it is valid, print "The number [card number] is valid!"
-## If it is invalid, print "The number [card number] is invalid!"
