75d90ead893530f8200fd488c36109dd811fad44
Sejin Kim <sejin.kim1904@gmail.com>
2020-05-26 13:03:42 -0600

    Add existance test

--- test/credit_card_test.rb
+++ test/credit_card_test.rb
@@ -7,4 +7,8 @@ class CreditCardTest < Minitest::Test
   def setup
     @credit_card = CreditCard.new("5541808923795240", 15000)
   end
+
+  def test_it_exists
+    assert_instance_of CreditCard, @credit_card
+  end
 end
