1fa28f59d4dade0b859a31fd298aaf59f911c5e3
Sejin Kim <sejin.kim1904@gmail.com>
2020-05-27 18:55:35 -0600

    Update is_valid? method to work correctly

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,11 +1,27 @@
 require './lib/credit_card'
 
-puts "Enter a credit card number you wish to validate:"
+def credit_check
+  puts "Enter a credit card number you wish to validate:"
 
-card = CreditCard.new(gets.chomp)
+  card = CreditCard.new(gets.chomp)
 
-if card.is_valid?
-  puts "The number #{card.card_number} is valid!"
-else
-  puts "The number #{card.card_number} is invalid!"
+  if card.is_valid?
+    puts "The number #{card.card_number} is valid!"
+  else
+    puts "The number #{card.card_number} is invalid!"
+  end
+
+  check_another
 end
+
+def check_another
+  puts "Would you like to check another card? (Y/N)"
+
+  answer = gets.chomp.capitalize
+
+  if answer == "Y"
+    credit_check
+  end
+end
+
+credit_check
